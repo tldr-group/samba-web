03395da013f2c994d59c671daec63c3e2db55412
rmdocherty <rmdocherty2@gmai.com>
2023-06-20 16:13:06 +0100

    More removals/ adjusts from auto save

--- frontend/src/components/helpers/Interfaces.tsx
+++ frontend/src/components/helpers/Interfaces.tsx
@@ -1,9 +1,3 @@
-// Copyright (c) Meta Platforms, Inc. and affiliates.
-// All rights reserved.
-
-// This source code is licensed under the license found in the
-// LICENSE file in the root directory of this source tree.
-
 import { Tensor } from "onnxruntime-web";
 
 export type Label = "Smart Labelling" | "Polygon" | "Brush" | "Erase"

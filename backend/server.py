2c0c2e634736bd28c1547e17fd1d78bdfe98c4d9
rmdocherty <rmdocherty2@gmai.com>
2024-08-27 12:37:39 +0100

    repr stuff - core + server changes

--- backend/server.py
+++ backend/server.py
@@ -10,13 +10,15 @@ from flask import (
     send_from_directory,
 )
 import base64
-from io import BytesIO
+from io import BytesIO, BufferedReader
 from PIL import Image
+import numpy as np
 from typing import Callable
 from azure.storage.blob import BlobServiceClient
 import os
 import json
 import zipfile
+from tifffile import imread
 
 from test_resources.call_weka import sep
 from encode import encode, featurise, imwrite
@@ -34,6 +36,7 @@ from file_handling import (
     _make_activity_log,
     _update_log,
 )
+from core import make_error_prediction
 
 # Very important: this environment variable is only present on webapp. If running locally, this fails and we use cwd instead.
 server = False
@@ -78,6 +81,8 @@ URL_WHITELIST = [
     "https://sambasegment.azureedge.net",
     "http://localhost:8080",
     "https://localhost:8080",
+    "https://www.imagerep.io",
+    "http://www.imagerep.io",
 ]
 
 
@@ -477,4 +482,94 @@ async def load_image(request) -> Response:
 @app.route("/lgallery", methods=["POST", "GET", "OPTIONS"])
 async def load_gallery_img_respond():
     response = await generic_response(request, load_image)
+    return response
+
+
+# ================================= NUMPY =================================
+def get_arr_from_file(form_data_file) -> np.ndarray:
+    user_filename = form_data_file.filename
+
+    file_object = BufferedReader(form_data_file)
+    # note we assume the .tiff or image is greyscale
+    if ".tif" in user_filename:
+        arr = imread(file_object)
+    else:
+        img = Image.open(file_object).convert("L")
+        arr = np.asarray(img)
+    arr = preprocess_arr(arr)
+    return arr
+
+
+async def phase_fraction(request) -> Response:
+    """User sends file, parse as array (either via tiffile or PIL-> numpy) and return all phase fractions.
+    It's cheap to compute all of them and this way it can be done on first upload in the background and
+    minimise delays."""
+    user_file = request.files["userFile"]
+    arr = get_arr_from_file(user_file)
+
+    out_fractions = {}
+    for val in np.unique(arr):
+        key = int(val)
+        out_fractions[key] = np.mean(arr == key)
+    print(f"Phase fractions: {out_fractions}")
+
+    obj = {"phase_fractions": out_fractions}
+    response = Response(json.dumps(obj), status=200)
+    return response
+
+
+@app.route("/phasefraction", methods=["POST", "GET", "OPTIONS"])
+async def phase_fraction_app():
+    """phase fraction route."""
+    response = await generic_response(request, phase_fraction)
+    return response
+
+
+def preprocess_arr(arr: np.ndarray) -> np.ndarray:
+    if len(arr.shape) == 3 and arr.shape[0] == 1:
+        # weird (1, H, W) tiffs
+        arr = arr[0, :, :]
+    if len(arr.shape) == 3 and arr.shape[-1] == 3:
+        # weird (H, W, 3) tiffs
+        arr = np.transpose(arr, (-1, 0, 1))
+    if len(arr.shape) == 3 and arr.shape[0] == 3:
+        # any (3, H, W) tiffs -> (H, W)
+        arr = arr[0, :, :]
+    return arr
+
+
+async def representativity(request) -> Response:
+    user_file = request.files["userFile"]
+    arr = get_arr_from_file(user_file)
+
+    selected_phase = int(request.values["selected_phase"])
+    selected_conf: float = float(request.values["selected_conf"]) / 100
+    selected_err: float = float(request.values["selected_err"]) / 100
+
+    print(f"Phase: {selected_phase}, Conf: {selected_conf}, Err: {selected_err}")
+
+    binary_img = np.where(arr == selected_phase, 1, 0)
+
+    result = make_error_prediction(
+        binary_img, selected_conf, selected_err, model_error=True
+    )  # make_error_prediction(binary_img, selected_conf, selected_err)
+    # this can get stuck sometimes in the optimisation step (usually cls > 1)
+    out = {
+        "abs_err": result["abs_err"],
+        "percent_err": result["percent_err"] * 100,
+        "std_model": result["std_model"],
+        "l": result["l"],
+        "cls": result["integral_range"],
+        "pf_1d": result["pf_1d"],
+        "cum_sum_sum": result["cum_sum_sum"],
+    }
+
+    response = Response(json.dumps(out), status=200)
+    return response
+
+
+@app.route("/repr", methods=["POST", "GET", "OPTIONS"])
+async def representativity_app():
+    """representativity route."""
+    response = await generic_response(request, representativity)
     return response
